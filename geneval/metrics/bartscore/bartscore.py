7c8ab8b82cae65b93c3e351b59864446b6ffd7d3
Robin Dreher <robin.dreher@web.de>
2022-08-09 17:57:45 +0200

    fix dtypes

--- geneval/metrics/bartscore/bartscore.py
+++ geneval/metrics/bartscore/bartscore.py
@@ -149,9 +149,9 @@ class NewMetric(datasets.Metric):
             features=datasets.Features({
                 'predictions': datasets.Value('string'),
                 'sources': datasets.Value('string'),
-                'model_type': dataset.Value('int'),
-                'max_length': datasets.Value('int'),
-                'batch_size': datasets.Value('int'),
+                'model_type': datasets.Value('string'),
+                'max_length': datasets.Value('int32'),
+                'batch_size': datasets.Value('int32'),
             }),
             # Homepage of the metric for documentation
             homepage="https://github.com/neulab/BARTScore",
